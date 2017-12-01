117d978e7e2cc63ffc4cf4278b1b2c8245936d00
Fenhl <fenhl@fenhl.net>
2017-12-01 02:20:15 +0000

    login: Include response type in debug info

--- gefolge_web/login.py
+++ gefolge_web/login.py
@@ -60,7 +60,7 @@ def setup(app, config):
         if flask_dance.contrib.discord.discord.authorized:
             response = flask_dance.contrib.discord.discord.get('/apt/v6/users/@me')
             if not response.ok:
-                return flask.make_response(('Discord returned error: {!r}'.format(response), 500, []))
+                return flask.make_response(('Discord returned error ({}): {!r}'.format(type(response), response), 500, []))
             flask_login.login_user(Mensch(response.json()['id']), remember=True)
             flask.flash('Hallo {}.'.format(response.json()['username']))
         else:
