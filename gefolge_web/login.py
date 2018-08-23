1d0eeac2a7e5f1b044ea7b7813688280347766d3
Fenhl <fenhl@fenhl.net>
2018-08-23 16:53:17 +0000

    event: Anmeldeformulare nach Ende nicht mehr anzeigen

--- gefolge_web/login.py
+++ gefolge_web/login.py
@@ -59,6 +59,10 @@ class Mensch(flask_login.UserMixin):
         return self.profile_path.exists() and MENSCHEN in self.data.get('roles')
 
     @property
+    def is_admin(self):
+        return self == self.__class__.admin()
+
+    @property
     def is_guest(self):
         return False
 
