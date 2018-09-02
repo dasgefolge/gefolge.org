5dffbd93895400d28b10688c3be1168712332d57
Fenhl <fenhl@fenhl.net>
2018-09-02 08:27:22 +0000

    event: Bessere Vorauswahl für Person, die für einen Programmpunkt angemeldet werden soll

--- gefolge_web/event/programm/__init__.py
+++ gefolge_web/event/programm/__init__.py
@@ -121,7 +121,7 @@ class Programmpunkt:
                 submit_text = self.data.get('signupOtherButton', '{} als interessiert markieren').format(more_itertools.one(people_allowed_to_sign_up))
             Form.submit_programmpunkt_form = wtforms.SubmitField(submit_text)
         else:
-            Form.person_to_signup = gefolge_web.forms.EventPersonField(self.event, 'Mensch', person_filter=lambda person: person in people_allowed_to_sign_up)
+            Form.person_to_signup = gefolge_web.forms.EventPersonField(self.event, 'Mensch', person_filter=lambda person: person in people_allowed_to_sign_up, default=editor if editor in people_allowed_to_sign_up else people_allowed_to_sign_up[0])
             submit_text = self.data.get('signupOtherButton', '{} als interessiert markieren').format('Gewählte Person')
 
         self.add_form_details(Form, editor)
