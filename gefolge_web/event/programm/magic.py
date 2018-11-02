03fb424d3355b34809bd8c465b784685ef732342
Fenhl <fenhl@fenhl.net>
2018-11-02 19:25:59 +0000

    Custom Magic Draft: Add support for closing format poll

--- gefolge_web/event/programm/magic.py
+++ gefolge_web/event/programm/magic.py
@@ -26,18 +26,32 @@ class CustomMagicDraft(gefolge_web.event.programm.Programmpunkt):
         return 'Custom Magic Draft'
 
     def add_form_details(self, Form, editor):
-        #TODO remove format poll when set has been determined
-        Form.section_sets_intro = gefolge_web.forms.FormText('Welche(s) set(s) würdest du am liebsten draften? Du kannst auch ohne abzustimmen einen Platz reservieren.')
-        for set_code, (set_info, set_config) in self.draftable_sets():
-            votes = list(map(gefolge_web.login.Mensch, self.data.get('votes', {}).get(set_code, [])))
-            setattr(Form, 'set_checkbox_{}'.format(set_code), wtforms.BooleanField(gefolge_web.util.render_template('event.custom-magic-draft-set-blurb', programmpunkt=self, set_code=set_code, set_info=set_info, set_config=set_config, votes=votes), default=editor.snowflake in self.data.get('votes', {}).get(set_code, [])))
+        if self.card_set is None:
+            Form.section_sets_intro = gefolge_web.forms.FormText('Welche(s) set(s) würdest du am liebsten draften? Du kannst auch ohne abzustimmen einen Platz reservieren.')
+            for set_code, (set_info, set_config) in self.draftable_sets():
+                votes = list(map(gefolge_web.login.Mensch, self.data.get('votes', {}).get(set_code, [])))
+                setattr(Form, 'set_checkbox_{}'.format(set_code), wtforms.BooleanField(gefolge_web.util.render_template('event.custom-magic-draft-set-blurb', programmpunkt=self, set_code=set_code, set_info=set_info, set_config=set_config, votes=votes), default=editor.snowflake in self.data.get('votes', {}).get(set_code, [])))
 
     def assert_exists(self):
         pass # assume each event has a Custom Magic Draft
 
     @property
+    def card_set(self):
+        for set_code, iter_set in MAGIC_CONFIG['customSets'].value():
+            if iter_set.get('drafted') == self.event.event_id:
+                return set_code
+
+    @property
     def description(self):
-        return 'Wir [draften](https://mtg.gamepedia.com/Booster_draft) ein Custom Magic Set. Um zu bestimmen, welches, kannst du unten abstimmen.\r\n\r\nWir spielen mit [Proxies](https://mtg.gamepedia.com/Proxy), der Draft ist also kostenlos. Ihr müsst nichts mitbringen. Es gibt 8 Plätze. Es können gerne alle, die Interesse haben, Plätze reservieren, das ist *keine* verbindliche Anmeldung. Ich selbst spiele nur mit, wenn es ohne mich weniger als 8 Spieler wären. Falls wir am Ende weniger als 5 Menschen sind, spielen wir [Sealed](https://mtg.gamepedia.com/Sealed_deck) statt Draft.'
+        set_code = self.card_set
+        if set_code is None:
+            result = 'Wir [draften](https://mtg.gamepedia.com/Booster_draft) ein Custom Magic Set. Um zu bestimmen, welches, kannst du unten abstimmen.'
+        else:
+            set_info = lazyjson.File(LORE_SEEKER_REPO / 'data' / 'sets' / '{}.json'.format(set_code))
+            set_config = MAGIC_CONFIG['customSets'].get(set_code, {})
+            result = 'Wir [draften](https://mtg.gamepedia.com/Booster_draft) [*{}*](https://loreseeker.fenhl.net/set/{}), ein Custom Magic Set.'.format(set_info['name'], set_code.lower())
+            result += '\r\n\r\n*{}* {}'.format(set_info['name'], set_config.get('blurb', 'hat noch keine Beschreibung :('))
+        return result + '\r\n\r\nWir spielen mit [Proxies](https://mtg.gamepedia.com/Proxy), der Draft ist also kostenlos. Ihr müsst nichts mitbringen. Es gibt 8 Plätze. Es können gerne alle, die Interesse haben, Plätze reservieren, das ist *keine* verbindliche Anmeldung. Ich selbst spiele nur mit, wenn es ohne mich weniger als 8 Spieler wären. Falls wir am Ende weniger als 5 Menschen sind, spielen wir [Sealed](https://mtg.gamepedia.com/Sealed_deck) statt Draft.'
 
     @description.setter
     def description(self, value):
@@ -65,20 +79,20 @@ class CustomMagicDraft(gefolge_web.event.programm.Programmpunkt):
             raise NotImplementedError()
 
     def process_form_details(self, form, editor):
-        #TODO disallow voting on the format poll when set has been determined
-        for set_code, (set_info, set_config) in self.draftable_sets():
-            if getattr(form, 'set_checkbox_{}'.format(set_code)).data:
-                # voted
-                if editor.snowflake not in self.data.get('votes', {}).get(set_code, []):
-                    if 'votes' not in self.data:
-                        self.data['votes'] = {}
-                    if set_code not in self.data['votes']:
-                        self.data['votes'][set_code] = []
-                    self.data['votes'][set_code].append(editor.snowflake)
-            else:
-                # not voted
-                if editor.snowflake in self.data.get('votes', {}).get(set_code, []):
-                    self.data['votes'][set_code] = list(filter(lambda snowflake: snowflake != editor.snowflake, self.data['votes'][set_code].value()))
+        if self.card_set is None:
+            for set_code, (set_info, set_config) in self.draftable_sets():
+                if getattr(form, 'set_checkbox_{}'.format(set_code)).data:
+                    # voted
+                    if editor.snowflake not in self.data.get('votes', {}).get(set_code, []):
+                        if 'votes' not in self.data:
+                            self.data['votes'] = {}
+                        if set_code not in self.data['votes']:
+                            self.data['votes'][set_code] = []
+                        self.data['votes'][set_code].append(editor.snowflake)
+                else:
+                    # not voted
+                    if editor.snowflake in self.data.get('votes', {}).get(set_code, []):
+                        self.data['votes'][set_code] = list(filter(lambda snowflake: snowflake != editor.snowflake, self.data['votes'][set_code].value()))
 
     @property
     def signup_limit(self):
