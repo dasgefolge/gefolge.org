0dd637278592b7600cb853afdb860e3724bdc731
Fenhl <fenhl@fenhl.net>
2023-03-07 03:10:43 +0000

    Allow testing on Windows

--- crate/front/src/config.rs
+++ crate/front/src/config.rs
@@ -1,15 +1,22 @@
 use {
     serde::Deserialize,
     serenity::model::prelude::*,
+};
+#[cfg(unix)] use {
     xdg::BaseDirectories,
     wheel::fs,
+};
+#[cfg(windows)] use {
+    tokio::process::Command,
+    wheel::traits::AsyncCommandOutputExt as _,
 };
 
 #[derive(Debug, thiserror::Error)]
 pub(crate) enum Error {
     #[error(transparent)] Json(#[from] serde_json::Error),
     #[error(transparent)] Wheel(#[from] wheel::Error),
-    #[error(transparent)] Xdg(#[from] xdg::BaseDirectoriesError),
+    #[cfg(unix)] #[error(transparent)] Xdg(#[from] xdg::BaseDirectoriesError),
+    #[cfg(unix)]
     #[error("missing config file")]
     Missing,
 }
@@ -23,11 +30,16 @@ pub(crate) struct Config {
 
 impl Config {
     pub(crate) async fn load() -> Result<Self, Error> {
-        if let Some(config_path) = BaseDirectories::new()?.find_config_file("gefolge.json") {
-            let buf = fs::read(config_path).await?;
-            Ok(serde_json::from_slice(&buf)?)
-        } else {
-            Err(Error::Missing)
+        #[cfg(unix)] {
+            if let Some(config_path) = BaseDirectories::new()?.find_config_file("gefolge.json") {
+                let buf = fs::read(config_path).await?;
+                Ok(serde_json::from_slice(&buf)?)
+            } else {
+                Err(Error::Missing)
+            }
+        }
+        #[cfg(windows)] { // allow testing without having rust-analyzer slow down production
+            Ok(serde_json::from_slice(&Command::new("ssh").arg("gefolge.org").arg("cat").arg("/etc/xdg/gefolge.json").check("ssh").await?.stdout)?)
         }
     }
 }
