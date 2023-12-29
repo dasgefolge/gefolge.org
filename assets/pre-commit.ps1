355f2242cc9ac1b3d4f321e954738d2e68de0c9a
Fenhl <fenhl@fenhl.net>
2023-12-29 14:38:45 +0000

    Enable compiling without access to the database

---
+++ assets/pre-commit.ps1
@@ -0,0 +1,21 @@
+#!/usr/bin/env pwsh
+
+function ThrowOnNativeFailure {
+    if (-not $?)
+    {
+        throw 'Native Failure'
+    }
+}
+
+cargo check
+ThrowOnNativeFailure
+
+cargo sqlx prepare --workspace --check
+ThrowOnNativeFailure
+
+# copy the tree to the WSL file system to improve compile times
+wsl rsync --delete -av /mnt/c/Users/fenhl/git/github.com/dasgefolge/gefolge.org/stage/ /home/fenhl/wslgit/github.com/dasgefolge/gefolge.org/ --exclude target
+ThrowOnNativeFailure
+
+wsl env -C /home/fenhl/wslgit/github.com/dasgefolge/gefolge.org cargo check --workspace
+ThrowOnNativeFailure
