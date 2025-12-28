870bc2e9d1141073242c832d37658436549af631
Fenhl <fenhl@fenhl.net>
2025-12-28 09:56:14 +0000

    Move access restriction for /games

--- gefolge_web/games.py
+++ gefolge_web/games.py
@@ -2,7 +2,8 @@ import gefolge_web.login
 import gefolge_web.util
 
 def setup(index):
-    @index.child('games', 'Spiele', decorators=[gefolge_web.login.mensch_required])
+    @index.child('games', 'Spiele')
+    @gefolge_web.login.mensch_required
     @gefolge_web.util.template('games-index')
     def games_index():
         return {}
