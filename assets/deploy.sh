04001f380263a004e176b4332b60af01652cc6d5
Fenhl <fenhl@fenhl.net>
2026-03-08 13:08:37 +0000

    deploy: Build on vendredi

--- assets/deploy.sh
+++ assets/deploy.sh
@@ -5,16 +5,20 @@ set -e
 if [[ x"$(hostname -f)" == x'vendredi.fenhl.net' ]]; then
     # deploy flask-view-tree
     echo 'deploying flask-view-tree'
-    cd /opt/git/github.com/fenhl/flask-view-tree/main
-    git --git-dir=/opt/git/github.com/fenhl/flask-view-tree/main/.git pull
+    env -C /opt/git/github.com/fenhl/flask-view-tree/main git --git-dir=/opt/git/github.com/fenhl/flask-view-tree/main/.git pull
     # deploy spacealert
     echo 'deploying spacealert'
-    cd /opt/git/github.com/dasgefolge/spacealert/main
-    git --git-dir=/opt/git/github.com/dasgefolge/spacealert/main/.git pull
+    env -C /opt/git/github.com/dasgefolge/spacealert/main git --git-dir=/opt/git/github.com/dasgefolge/spacealert/main/.git pull
     # deploy gefolge.org
     echo 'deploying gefolge.org'
-    cd /opt/git/github.com/dasgefolge/gefolge.org/main
-    git --git-dir=/opt/git/github.com/dasgefolge/gefolge.org/main/.git pull
+    rustup update stable
+    #TODO cargo sweep (limit to once per Rust version)
+    cargo build --release --package=gefolge-web --package=gefolge-web-back
+    sudo systemctl stop gefolge-web
+    env -C /opt/git/github.com/dasgefolge/gefolge.org/main git --git-dir=/opt/git/github.com/dasgefolge/gefolge.org/main/.git pull
+    cp target/release/gefolge-web /home/fenhl/bin/gefolge-web
+    cp target/release/gefolge-web-back ~/bin/gefolge-web-back
+    sudo systemctl start gefolge-web
     # restart nginx (since nginx config is tracked by git) and uWSGI
     sudo systemctl daemon-reload
     sudo systemctl reload nginx
